5ccc27c04fa59ebdea3cbc726b28ce3df8d17b1b
Tamerlan <59252765+farzullali@users.noreply.github.com>
2025-05-06 08:54:32 +0400

    commit

--- order-service/dist/orders/orders.controller.d.ts
+++ order-service/dist/orders/orders.controller.d.ts
@@ -3,32 +3,32 @@ export declare class OrdersController {
     private readonly ordersService;
     constructor(ordersService: OrdersService);
     create(data: any): Promise<{
-        id: string;
         title: string;
         description: string;
         price: number;
-        userId: string;
         orderReference: string | null;
+        id: string;
+        userId: string;
         createdAt: Date;
         updatedAt: Date;
     }>;
     findAll(data: any): Promise<{
-        id: string;
         title: string;
         description: string;
         price: number;
-        userId: string;
         orderReference: string | null;
+        id: string;
+        userId: string;
         createdAt: Date;
         updatedAt: Date;
     }[]>;
     findOne(data: any): Promise<{
-        id: string;
         title: string;
         description: string;
         price: number;
-        userId: string;
         orderReference: string | null;
+        id: string;
+        userId: string;
         createdAt: Date;
         updatedAt: Date;
     }>;
